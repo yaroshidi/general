c7c6812e774f3c1fd78402a2e9d302c6101ce918
The Mesopotamian <55325440+yaroshidi@users.noreply.github.com>
2024-10-25 15:41:58 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,39 @@
+gsap.registerPlugin(ScrollTrigger);
+
+let tl = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".layout417_component",
+    start: "top top",
+    end: "bottom bottom",
+    scrub: true,
+  },
+});
+
+// Card 1 animation
+tl.to(".card-1", {
+  y: "-100vh",
+  rotation: -30,
+  duration: 1,
+  ease: "power2.in",
+});
+// Card 2 animation
+tl.to(".card-2", {
+  y: "-100vh",
+  rotation: -30,
+  duration: 1,
+  ease: "power2.in",
+});
+// Card 3 animation
+tl.to(".card-3", {
+  y: "-100vh",
+  rotation: -30,
+  duration: 1,
+  ease: "power2.in",
+});
+// Card 4 animation
+tl.to(".card-4", {
+  y: "-100vh",
+  rotation: -30,
+  duration: 1,
+  ease: "power2.in",
+});
